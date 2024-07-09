120b880eca9739c9e44bb22ee2d140c356b048fe
Dani0986 <daniosr1986@gmail.com>
2024-07-09 21:16:18 +0200

    new

--- src/context/CharacterContext.jsx
+++ src/context/CharacterContext.jsx
@@ -1,13 +1,17 @@
+// characters/CharactersContext.js
 import React, { createContext, useEffect, useState } from 'react';
 import axios from 'axios';
 
+// Creamos el contexto
 export const CharactersContext = createContext();
 
+// Proveedor de contexto para los personajes
 export const CharactersProvider = ({ children }) => {
   const [characters, setCharacters] = useState([]);
 
+  // Función para obtener los personajes desde la API
   useEffect(() => {
-    const obtenerCharacters = async () => {
+    const fetchCharacters = async () => {
       try {
         const response = await axios.get('http://localhost:8080/api/v1/character/getAll');
         setCharacters(response.data);
@@ -16,9 +20,10 @@ export const CharactersProvider = ({ children }) => {
       }
     };
 
-    obtenerCharacters();
+    fetchCharacters();
   }, []);
 
+  // Proporcionamos los personajes a través del contexto
   return (
     <CharactersContext.Provider value={characters}>
       {children}
